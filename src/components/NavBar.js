3b3952d9d9f8cd6dd2ef428df323c728f4219708
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-01 21:46:44 +0530

    Imported the custom fonts

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -27,7 +27,7 @@ export const NavBar = () => {
     return (
         <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
         <Container>
-          <Navbar.Brand href="#home"><img src ={''} alt="Logo" /></Navbar.Brand>
+          <Navbar.Brand href="#home"><img src ={logo} alt="Logo" /></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
