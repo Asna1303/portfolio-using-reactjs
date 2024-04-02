8715bd12664953c5e5bd73f51ebd47d61af62618
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-02 23:28:11 +0530

    implemented container,row from bootstrap

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,11 @@
 export const Banner = () => {
     return (
        <section className="banner" id="home">
-        
+        <Container>
+            <Row>
+                
+            </Row>
+        </Container>
 
        </section> 
     )
