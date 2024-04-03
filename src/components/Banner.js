02a610b4badcd9c6bc0200db1c1a35cf69abe17e
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-03 10:20:55 +0530

    Added Row component with className attribute for aligning items, and Col components with specified width breakpoints (xs=12, md=6, xl=7).

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,8 +2,8 @@ export const Banner = () => {
     return (
        <section className="banner" id="home">
         <Container>
-            <Row>
-                <Col>
+            <Row className ="aligh-items-center">
+                <Col xs={12} md={6} xl={7}>
                 </Col>
             </Row>
         </Container>
