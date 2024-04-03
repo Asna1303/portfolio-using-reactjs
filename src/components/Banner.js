087f1e1a0b87a70c42df49af09580cb594a3d4f1
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-03 10:26:07 +0530

    given the h1 component with animation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,6 +5,7 @@ export const Banner = () => {
             <Row className ="aligh-items-center">
                 <Col xs={12} md={6} xl={7}>
                 <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm ASNA`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
                 </Col>
             </Row>
         </Container>
