5a63f8ddcbe15f788b7735ef3f5a78af85db5a94
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-06 21:18:05 +0530

    imported container,row,col from react bootstrap

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,28 +1,33 @@
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import {Container, Row,Col} from "react-bootstrap";
 export const Skills = () => {
-    const responsive = {
-        superLargeDesktop: {
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-    };
-    return (
-        <section className="skill" id="skills">
-           
-        </section>
-    );
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+  return (
+    <section className="skill" id="skills">
+      <Container>
+        <Row>
+          <Col>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
