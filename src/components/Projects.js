1a9c0944fc8406745987d1906a689a9310580d1c
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-07 01:11:43 +0530

    added the bootstrap code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -42,6 +42,18 @@ export const Projects = () => {
                 <Row>
                     <Col>
                     <h2>Projects</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
                     </Col>
                 </Row>
             </Container>
