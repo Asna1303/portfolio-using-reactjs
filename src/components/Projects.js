3557b53aaeae08b7c704221c34ba91b3f4e7a122
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-07 01:00:14 +0530

    Imported images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,7 @@
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+
 export const Projects = () => {
     
     const projects = [
@@ -33,6 +37,7 @@ export const Projects = () => {
           }, 
     ];
     return (
-
+        <section className="project" id="projects">
+            </section>
     )
 }
