36da110270da673701d2c4c64e03f4fc349f3823
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-07 14:01:09 +0530

    added the tab pane componenet

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -56,6 +56,9 @@ export const Projects = () => {
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                  <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
                     </Tab.Content>
                   </Tab.Container>
                     </Col>
