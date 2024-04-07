0106df530c3c3964bbb99efd1898199ab10a08be
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-07 14:08:32 +0530

    imported projectcard

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
 export const Projects = () => {
     
     const projects = [
