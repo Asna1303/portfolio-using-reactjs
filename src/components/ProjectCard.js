c2cf950f4adee71f93d02ea684a8bbc0b9df6336
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-08 08:00:30 +0530

    added class called proj-txtx

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,6 +3,8 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
         <Col size={12} sm={6} md={4}>
              <div className="proj-imgbx">
              <img src={imgUrl} />
+             <div className="proj-txtx">
+                </div>
              </div>
         </Col>
     )
