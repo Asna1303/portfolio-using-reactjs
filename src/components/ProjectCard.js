8228f792d14d92012381ccd51d2dd250b87367e3
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-08 07:59:00 +0530

    added the img src

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,7 +2,7 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
     return (
         <Col size={12} sm={6} md={4}>
              <div className="proj-imgbx">
-                
+             <img src={imgUrl} />
              </div>
         </Col>
     )
