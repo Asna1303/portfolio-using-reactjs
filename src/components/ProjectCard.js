32a3f8cea1c48bf4ad687081874e08c0143039b7
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-08 07:58:01 +0530

    created a class called projimgbx

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,9 @@
 export const ProjectCard = ({ title, description, imgUrl }) => {
     return (
         <Col size={12} sm={6} md={4}>
+             <div className="proj-imgbx">
+                
+             </div>
         </Col>
     )
 }
